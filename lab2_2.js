bdcbe217ada71e899367931200234b30765f172d
AlenaPanteleeva <pantalena7773@gmail.com>
2024-10-04 16:56:04 +0700

    Delete lab2_2.js

--- lab2_2.js
+++
@@ -1,9 +0,0 @@
-let number = prompt("Введите целое положительное трехзначное число")
-
-let lastNumber = number % 10; // последняя цифра
-let middleNumber = ((number % 100) / 10); // средняя цифра
-
-alert("Последняя цифра: " + lastNumber);
-alert("Средняя цифра: " + middleNumber);
-
-
